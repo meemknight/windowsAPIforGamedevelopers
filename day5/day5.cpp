709c8fee50a1cfeaabbd371bab98e8ce15d24b68
meemknight <vladalex420@gmail.com>
2023-09-09 13:54:33 +0300

    fixes

--- day5/day5.cpp
+++ day5/day5.cpp
@@ -9,14 +9,13 @@ struct Test
 	int b = 0;
 };
 
-
 size_t getFileSize(std::string name);
 bool readEntireFileBinary(std::string name, std::vector<unsigned char> &data);
-bool writeEntireFileBinary(std::string_view name, void *buffer, size_t s);
+bool writeEntireFileBinary(std::string name, void *buffer, size_t s);
 
 int main()
 {
-	
+
 	Test t;
 	t.a = 10.8;
 	t.b = 5;
@@ -28,14 +27,14 @@ int main()
 
 	std::vector<unsigned char> data;
 	readEntireFileBinary(FILE_PATH "test.bin", data);
-	
-	if(sizeof(t) == data.size())
+
+	if (sizeof(t) == data.size())
 		std::memcpy(&t, data.data(), sizeof(t));
-	
+
 	std::cout << t.a << " " << t.b << "  file size bytes: " << getFileSize(FILE_PATH "test.bin");
 	std::cin.get();
 	return 0;
-}	
+}
 
 
 size_t getFileSize(std::string name)
@@ -82,13 +81,13 @@ bool readEntireFileBinary(std::string name, std::vector<unsigned char> &data)
 	}
 
 	data.resize(fileSize.QuadPart);
-	
+
 	DWORD bytesRead = 0;
 	bool rez = ReadFile(file, data.data(), fileSize.QuadPart, &bytesRead, nullptr);
 
 	CloseHandle(file);
 
-	if (!rez || bytesRead == fileSize.QuadPart)
+	if (!rez || bytesRead != fileSize.QuadPart)
 	{
 		return 0;
 	}
@@ -99,23 +98,31 @@ bool readEntireFileBinary(std::string name, std::vector<unsigned char> &data)
 
 }
 
-bool writeEntireFileBinary(std::string_view name, void *buffer, size_t s)
+bool writeEntireFileBinary(std::string name, void *buffer, size_t s)
 {
-	HANDLE file = CreateFile(name.data(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	//no share mode because we are modifying it
+	HANDLE file = CreateFile(name.c_str(), GENERIC_WRITE, 0,
+		NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
 	if (file == INVALID_HANDLE_VALUE)
 	{
-		// log error here
+		//log err
 		return false;
 	}
 
 	if (!buffer || s == 0)
 	{
+		//no file content nothing to write
 		CloseHandle(file);
 		return true;
 	}
 
-	DWORD bytesWritten;
-	bool success = WriteFile(file, buffer, static_cast<DWORD>(s), &bytesWritten, NULL) && bytesWritten == s;
+	DWORD bytesWritten = 0;
+	bool success = WriteFile(file, buffer, s, &bytesWritten, NULL);
+
+	if (bytesWritten != s) { success = false; }
+
 	CloseHandle(file);
+
 	return success;
 }
