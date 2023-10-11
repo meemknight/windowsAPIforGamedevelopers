7eb8a5aaf7a831e7ed989e4994c0ee95b8472d85
meemknight <vladalex420@gmail.com>
2023-10-11 12:05:05 +0300

    update

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -3,6 +3,7 @@
 
 SYSTEM_INFO sSysInfo;
 
+//rounds up the value to be multiple of page size
 size_t roundUP(size_t in)
 {
 	if (in == 0) { return 0; }
@@ -21,28 +22,27 @@ size_t roundUP(size_t in)
 
 int main()
 {
+	//get the page size info
 	GetSystemInfo(&sSysInfo);
 
 	std::cout << "Page size: " << sSysInfo.dwPageSize << "\n";
 	std::cout << "Round Up test: " << roundUP(5000) << "\n";
 
-	void *memory = VirtualAlloc((void*)0xffff0001, 200, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	void *memory = VirtualAlloc(0, 200,
+		MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 
 	if (memory)
 	{
 		std::cout << memory << "\n";
 
 		((unsigned char *)memory)[300] = 5;
-
 		
-		std:: cout << (int)((unsigned char *)memory)[300] << "\n";
+		std::cout << (int)((unsigned char *)memory)[300] << "\n";
 
-
-		//guarding the page
+		//guarding the page example
 		DWORD old = 0;
 		VirtualProtect(memory, roundUP(200), PAGE_NOACCESS, &old);
-		
-		//this will crash the program
+		//this would crash the program if called
 		//((unsigned char *)memory)[300] = 6;
 
 		VirtualFree(memory, 0, MEM_RELEASE);
@@ -50,6 +50,8 @@ int main()
 
 
 	std::cin.get();
-	
+
 	return 0;
-}
+};
+
+
